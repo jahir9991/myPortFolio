23a16a19252625d83bfb24701a30b89d4e6aba9e
jahir9991 <work.jahiralam@gmail.com>
2018-05-07 00:47:13 +0600

    first commit

---
+++ src/app/@core/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker'
+
+import { AppComponent } from './app.component';
+import {UiModule} from '../@modules/ui/ui.module';
+import {environment} from '../../environments/environment';
+import {AboutComponent} from '../@pages/about/about.component';
+import {WorkComponent} from '../@pages/work/work.component';
+import {HomeComponent} from '../@pages/home/home.component';
+import {AppRoutingModule} from './approuting.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    WorkComponent
+  ],
+  imports: [
+    // environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
+    //  ServiceWorkerModule.register('/ngsw-worker.js') ,
+    BrowserModule,
+    AppRoutingModule,
+    UiModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
